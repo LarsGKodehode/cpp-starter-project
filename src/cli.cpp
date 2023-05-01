26def03833699288cf72736317d055ceae42b06a
LarsG <lars.gunnar.kodehodet@gmail.com>
2023-05-01 22:10:00 +0200

    add comment about argv type

--- src/cli.cpp
+++ src/cli.cpp
@@ -9,6 +9,9 @@ int main(int argc, char const *argv[])
   // argc is the number of arguments passed to the program
   // argv is a vector with the values of those arguments
 
+  // Also mind the type of argv, it is possible to convert it to other types,
+  // but you will have to jump trough a couple of hoops to achieve it
+
   // First entry of argv is the name of the program
   LOG(argv[0]);
 
